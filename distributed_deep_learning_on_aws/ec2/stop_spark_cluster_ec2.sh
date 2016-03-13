15c1c84d64a018c04c03ee1533f20fc42875c925
Vincent Van Steenbergen <v.vansteenbergen@gmail.com>
2016-03-13 15:40:25 +0000

    Remove reveal submodul

--- distributed_deep_learning_on_aws/ec2/stop_spark_cluster_ec2.sh
+++
@@ -1,4 +0,0 @@
-. ./spark_ec2_config.sh
-./spark-ec2 --key-pair=${EC2_KEY} --identity-file=${EC2_PEM_FILE} \
-                --region=${EC2_REGION} --zone=${EC2_ZONE} \
-                destroy ${SPARK_CLUSTER_NAME}
